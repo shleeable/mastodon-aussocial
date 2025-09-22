7acfe88005bc033700d31b40b2f765317dd60153
Shlee <github@shl.ee>
2025-09-23 09:39:24 +1000

    Update status.jsx

--- app/javascript/mastodon/components/status.jsx
+++ app/javascript/mastodon/components/status.jsx
@@ -74,7 +74,7 @@ export const defaultMediaVisibility = (status) => {
 
 const messages = defineMessages({
   public_short: { id: 'privacy.public.short', defaultMessage: 'Public' },
-  unlisted_short: { id: 'privacy.unlisted.short', defaultMessage: 'Public Unlisted' },
+  unlisted_short: { id: 'privacy.unlisted.short', defaultMessage: 'Quiet public' },
   private_short: { id: 'privacy.private.short', defaultMessage: 'Followers' },
   direct_short: { id: 'privacy.direct.short', defaultMessage: 'Specific people' },
   edited: { id: 'status.edited', defaultMessage: 'Edited {date}' },
