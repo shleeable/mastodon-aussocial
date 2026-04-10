8141188fd26d343f904b2f077b24a11db8f5779a
Shlee <github@shl.ee>
2026-04-10 22:04:45 +0930

    Update stoplight.rb

--- config/initializers/stoplight.rb
+++ config/initializers/stoplight.rb
@@ -4,7 +4,7 @@ require 'stoplight'
 
 Rails.application.reloader.to_prepare do
   Stoplight.configure do |config|
-    config.data_store = Stoplight::DataStore::Redis.new(RedisConnection.new.connection)
+    config.data_store = Stoplight::DataStore::Redis.new(RedisConnection.pool)
     config.notifiers = [Stoplight::Notifier::Logger.new(Rails.logger)]
   end
 end
